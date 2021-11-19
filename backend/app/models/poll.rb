630da5d5dbf3426cd9321cc2fb8a856d08051e6a
faez <faezc@hotmail.com>
2021-11-19 00:44:50 +0000

    Created users model and controller in addition to establishing relationships between the users and polls tables

--- backend/app/models/poll.rb
+++ backend/app/models/poll.rb
@@ -1,2 +1,5 @@
 class Poll < ActiveRecord::Base
+
+  has_many :users
+
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,3 +10,5 @@ Poll.create(restaurant_name_1: 'Claudia Kitchen1', restaurant_name_2: 'Cafe a la
 Poll.create(restaurant_name_1: 'Claudia Kitchen2', restaurant_name_2: 'Cafe a la Cynthia 2', restaurant_name_3: "Korean Village 2", votes_choice_1: 1, votes_choice_2: 2, votes_choice_3: 3)
 Poll.create(restaurant_name_1: 'Claudia Kitchen3', restaurant_name_2: 'Cafe a la Cynthia 3', restaurant_name_3: "Korean Village 3", votes_choice_1: 1, votes_choice_2: 2, votes_choice_3: 3)
 Poll.create(restaurant_name_1: 'Claudia Kitchen4', restaurant_name_2: 'Cafe a la Cynthia 4', restaurant_name_3: "Korean Village 4", votes_choice_1: 1, votes_choice_2: 2, votes_choice_3: 3)
+
+
