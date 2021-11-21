cb0f1b2a9964d1c25bc558d127c33a4d86539d75
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-11-21 14:54:10 -0800

    create Results and SingleResult components to render API data, currently hard-coded for testing purpose

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,6 +1,19 @@
+import { Fragment } from "react";
+import SingleResult from "./SingleResult";
+
 export default function Results(props) {
   return (
-    <div>"hello, welcome to your results page"</div>
-    //console.log("hello, welcome to your results page")
+    <Fragment>
+      <div className="page-number-display">
+        4 of 4
+      </div>
+      <h1>Your Customized Selections</h1>
+      <div>
+        <SingleResult/>
+        <SingleResult/>
+        <SingleResult/>
+      </div>
+    </Fragment>
  )
 }
+
